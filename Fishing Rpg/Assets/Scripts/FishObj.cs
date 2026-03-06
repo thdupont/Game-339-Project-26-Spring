7798fae8bd67244901b8529b64e61a2b7cb16e3d
William Douglas <willstaleydouglas@gmail.com>
2026-03-05 20:59:52 -0800

    Merging back to main

---
+++ Fishing Rpg/Assets/Scripts/FishObj.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FishObj", menuName = "Scriptable Objects/FishObj")]
+public class FishObj : ScriptableObject
+{
+    public float health;
+    public float maxHealth;
+    
+}
