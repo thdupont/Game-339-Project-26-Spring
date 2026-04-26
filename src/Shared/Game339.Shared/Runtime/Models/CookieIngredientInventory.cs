fec680edb44a153993815e78a8b81c60e0a2f874
Tom <red.shirt083@passinbox.com>
2026-04-26 13:21:56 -0700

    Refactored cookie labels

--- src/Shared/Game339.Shared/Runtime/Models/CookieIngredientInventory.cs
+++ src/Shared/Game339.Shared/Runtime/Models/CookieIngredientInventory.cs
@@ -16,11 +16,5 @@ namespace Game339.Shared.Models
 
         public int GetCount(CookieIngredient ingredient) =>
             Counts.TryGetValue(ingredient, out var obs) ? obs.Value : 0;
-
-        public ObservableValue<int> Chocolate    => Counts[CookieIngredient.Chocolate];
-        public ObservableValue<int> Nuts         => Counts[CookieIngredient.Nuts];
-        public ObservableValue<int> PeanutButter => Counts[CookieIngredient.PeanutButter];
-        public ObservableValue<int> Butterscotch => Counts[CookieIngredient.Butterscotch];
-        public ObservableValue<int> Sugar        => Counts[CookieIngredient.Sugar];
     }
 }
---
+++ src/UnityGame339/Assets/Game/Runtime/CookieIngredientLabelView.cs
@@ -0,0 +1,31 @@
+using System;
+using Game339.Shared.Models;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime
+{
+    public class CookieIngredientLabelView : ObserverMonoBehaviour
+    {
+        [SerializeField] private TMP_Text label;
+        [SerializeField] private string ingredientName;
+
+        private CookieIngredient _ingredient;
+
+        private static CookieIngredientInventory Inventory =>
+            ServiceResolver.Resolve<CookieIngredientInventory>();
+
+        protected override void Subscribe()
+        {
+            _ingredient = (CookieIngredient)Enum.Parse(typeof(CookieIngredient), ingredientName);
+            Inventory.Counts[_ingredient].ChangeEvent += OnChange;
+        }
+
+        protected override void Unsubscribe()
+        {
+            Inventory.Counts[_ingredient].ChangeEvent -= OnChange;
+        }
+
+        private void OnChange(int value) => label.text = ingredientName + ": " + value;
+    }
+}
